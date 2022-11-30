0ba99611dd7b11137448f51f20d3bb41c509c3c9
cornyyumi <cornyyumi@gmail.com>
2022-11-30 14:30:11 +1300

    Implemented Home page without links + polariod page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Home from './pages/Home';
 import Polariod from './pages/Polariod';
+import MyRoom from './pages/MyRoom';
+
 import reportWebVitals from './reportWebVitals';
 
 
@@ -11,8 +13,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="polariod" element={<Polariod />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="polariod" element={<Polariod/>} />
+        <Route path="myroom" element={<MyRoom/>} />
+
       </Routes>
     </BrowserRouter>
   );
